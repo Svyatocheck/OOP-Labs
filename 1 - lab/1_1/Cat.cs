62bd3c16b149db1f0b7531227d22f5386295eee0
svyatoslav567 <svyatwork3@yahoo.com>
2021-12-07 07:48:00 +0300

    New lab

---
+++ 1 - lab/1_1/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cats
+{
+    class Cat
+    {
+        private int its_age;
+
+        public int Age
+        {
+            get { return its_age; }
+            set { its_age = value;}
+        }
+
+        public Cat(int its_age)
+        {
+            Age = its_age;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+    }
+}
---
+++ 1 - lab/1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cats;
+
+// namespace declaration
+namespace Cats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat Misha = new Cat(2);
+
+            Console.WriteLine($"Misha is {Misha.Age} years old!");
+            Misha.Meow();
+
+            Misha.Age = 5;
+            Console.WriteLine($"Misha is {Misha.Age} years old!");
+            Misha.Meow();
+
+        }
+
+
+    }
+}
